0ac4b9fb1e5a2d38dd7ecde54c1a1055954a3ec5
Igor Shevchenko <bnzone@gmail.com>
2021-03-16 18:37:41 -0400

    Updates

---
+++ precache-manifest.bfc469c20d6b6f080a3490965afad001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "367077e885ee5b7572e6",
+    "url": "/static/css/main.d4bd6063.chunk.css"
+  },
+  {
+    "revision": "367077e885ee5b7572e6",
+    "url": "/static/js/main.367077e8.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57f9fb1ec2894d68e9f2ce8224fc7e3f",
+    "url": "/index.html"
+  }
+];
